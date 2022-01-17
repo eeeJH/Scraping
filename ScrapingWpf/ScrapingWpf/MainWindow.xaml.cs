3c9438f8740b88552cb899b579e1d3ce52480bac
이주현 <juhyeon.lee@seeroo.co.kr>
2022-01-17 14:22:49 +0900

    Sample Test

--- ScrapingWpf/ScrapingWpf/MainWindow.xaml.cs
+++ ScrapingWpf/ScrapingWpf/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,6 +19,7 @@ using System.Windows.Shapes;
 // Selenium
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chromium;
 using OpenQA.Selenium.Support.UI;
 
 namespace ScrapingWpf
@@ -29,13 +33,15 @@ namespace ScrapingWpf
         {
             InitializeComponent();
 
+            ChromiumNetworkConditions conditions = new ChromiumNetworkConditions();
+
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.reverbnation.com/");
 
             var discoverButton = driver.FindElement(By.Id("menu-item-discover"));
             discoverButton.Click();
 
-            var collections = driver.FindElements(By.ClassName("card__contents"));
+            var collections = FindElements(By.ClassName("card__contents"));
 
             foreach (var collection in collections)
             {
@@ -43,9 +49,23 @@ namespace ScrapingWpf
             }
         }
 
-        public void Scraping() { }
+        static IReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            Stopwatch w = Stopwatch.StartNew();
 
+            while (w.ElapsedMilliseconds < 10 * 1000)
+            {
 
+                var elements = driver.FindElements(by);
+
+                if (elements.Count > 0) { return elements; }
+
+                Thread.Sleep(10);
+            }
+
+            //return null;
+            return new ReadOnlyCollection<IWebElement>(new List<IWebElement>());
+        }
 
     }
 }
